449019878c3866c95e5cf7fe4f6eb7ab16b71608
akasugin <1750087920@qq.com>
2018-03-21 15:30:45 +0800

    change Controller to RestController

--- src/main/java/com/express/demo/controller/ExpressQueryController.java
+++ src/main/java/com/express/demo/controller/ExpressQueryController.java
@@ -1,12 +1,12 @@
 package com.express.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 public class ExpressQueryController {
     @RequestMapping(value = "/expressQuery")
     public String expressQuery(@RequestBody String data) throws IOException {
